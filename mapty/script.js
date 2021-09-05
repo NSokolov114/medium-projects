5e5d4865eccebb0fcf2fcc544976db0b63b4645f
NSokolov114 <sokoloff114@gmail.com>
2021-09-05 22:02:40 +0300

    feat grab user coordinates for mapty project

--- mapty/script.js
+++ mapty/script.js
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  // if it exists (check for old browsers)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // in case of success
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      // in case of failure
+      alert('Could not get your position');
+    }
+  );
