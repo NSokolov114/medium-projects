b6d9c35bf3f30b438b08d935b6d305477ba8cc9e
NSokolov114 <sokoloff114@gmail.com>
2021-10-18 23:11:18 +0300

    add the map for the park website

--- park-website/script.js
+++ park-website/script.js
@@ -4,4 +4,13 @@ document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
   console.log('hello there!');
+
+  // map for the contacts page
+  var mymap = L.map('mapid').setView([49.621, 18.915], 16);
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(mymap);
+  var marker = L.marker([49.621, 18.915]).addTo(mymap);
+  marker.bindPopup('<b>Hello world!</b><br>I am a popup.');
 }
