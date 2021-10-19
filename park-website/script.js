ac265d871408195cb89d334f3d876362171ebe89
NSokolov114 <sokoloff114@gmail.com>
2021-10-19 18:27:35 +0300

    feat about us module for the park website

--- park-website/script.js
+++ park-website/script.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', ready);
 function ready() {
   console.log('hello there!');
 
-  // map for the contacts page
-  var mymap = L.map('mapid').setView([49.621, 18.915], 16);
+  // map for the about us page
+  var mymap = L.map('mapid').setView([49.544, 18.684], 16);
   L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(mymap);
-  var marker = L.marker([49.621, 18.915]).addTo(mymap);
-  marker.bindPopup('<b>Hello world!</b><br>I am a popup.');
+  var marker = L.marker([49.544, 18.684]).addTo(mymap);
+  marker.bindPopup('Park entrance');
 }
