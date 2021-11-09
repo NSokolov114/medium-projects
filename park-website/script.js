ab2431321ce33da08fd6c76a8003c15cbeecbeae
NSokolov114 <sokoloff114@gmail.com>
2021-11-09 18:12:49 +0300

    feat map markers

--- park-website/script.js
+++ park-website/script.js
@@ -3,14 +3,16 @@
 document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
-  console.log('hello there!');
-
   // Leaflet JS map and markers
+  function createMarker(coords, text) {
+    var marker = L.marker(coords).addTo(myMap);
+    marker.bindPopup(`${text}`);
+    return marker;
+  }
+
   const overviewLocationBtns = document.querySelectorAll(
     '.overview__location-btn'
   );
-
-  overviewLocationBtns[0].addEventListener('click', () => {});
 
   const locations = [
     { description: 'Park entrance & reception', coords: [49.494, 18.472] },
@@ -27,14 +29,6 @@ function ready() {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(myMap);
-  // var markerEntrance = L.marker([49.544, 18.684]).addTo(myMap);
-  // markerEntrance.bindPopup('Reception').openPopup();
-
-  // createMarker(coordsArr[0], 'Reception').openPopup();
-  // coordsArr.forEach((coords, idx) => {
-  //   createMarker(coords, `spot ${idx}`);
-  //   console.log(coords, idx);
-  // });
 
   const markerEntrance = createMarker(
     locations[0].coords,
@@ -42,11 +36,14 @@ function ready() {
   );
   markerEntrance._icon.classList.add('entranceIcon');
 
-  function createMarker(coords, text) {
-    var marker = L.marker(coords).addTo(myMap);
-    marker.bindPopup(`${text}`);
-    return marker;
-  }
+  overviewLocationBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+      createMarker(
+        locations[idx + 1].coords,
+        locations[idx + 1].description
+      ).openPopup();
+    });
+  });
   ///////////////////////
 
   // RND gallery images order
@@ -67,8 +64,7 @@ function ready() {
   galleryImgs.forEach(function (el, idx) {
     el.src = `./img/gallery/gallery-${galleryRndOrder[idx]}.jpg`;
   });
-
-  ////////
+  /////////////////////////
 
   ///// generating random stuff /////
   // rating, number of votes
@@ -86,18 +82,12 @@ function ready() {
   }
 
   // BACK TO TOP BUTTON
-  //Get the button:
   const toTopBtn = document.querySelector('.to-top-btn');
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
 
   function scrollFunction() {
     if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
     ) {
       toTopBtn.classList.remove('hidden');
     } else {
@@ -105,11 +95,14 @@ function ready() {
     }
   }
 
-  // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
   toTopBtn.addEventListener('click', topFunction);
 }
