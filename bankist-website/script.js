9622a26c9e3edbd92f105f1a33eb5551b4697f6d
NSokolov114 <sokoloff114@gmail.com>
2022-01-27 23:35:54 +0300

    feat smoothing scroll for nav

--- bankist-website/script.js
+++ bankist-website/script.js
@@ -1,13 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -30,10 +31,52 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+///////////////////////////////////////
+// Smooth scrolling
+
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(window.pageYOffset);
+  // console.log(window.scrollY); // viewport Y offset in px
+
+  // window.scrollTo({
+  //   left: s1coords.left,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////
+// Page Navigation (event delegation)
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     // console.log(e.target.href);
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+////////////////////////////////////////////
+/////////// EXPERIMENTATION ////////////////
+////////////////////////////////////////////
+
 /////////// selecting elements
-console.log(document.documentElement);
-console.log(document.body);
-console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body);
+// console.log(document.head);
 
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
@@ -60,40 +103,63 @@ document
 // styles
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
-console.log(message.style.width); // '120%'
-console.log(message.style.height); // ''
-console.log(getComputedStyle(message).height); // '49.53px'
+// console.log(message.style.width); // '120%'
+// console.log(message.style.height); // ''
+// console.log(getComputedStyle(message).height); // '49.53px'
 
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 // attributes
 const logo = document.querySelector('.nav__logo');
-console.log(logo.src); // http://127.0.0.1:5500/img/logo.png
-console.log(logo.getAttribute('src')); // img/logo.png
-console.log(logo.className); // not logo.class!
-console.log(logo.getAttribute('myNameIs')); // Slim
+// console.log(logo.src); // http://127.0.0.1:5500/img/logo.png
+// console.log(logo.getAttribute('src')); // img/logo.png
+// console.log(logo.className); // not logo.class!
+// console.log(logo.getAttribute('myNameIs')); // Slim
 
 logo.alt = 'my best logo';
 logo.setAttribute('company', 'Bankist');
-console.log(logo.dataset.versionNumber); // 3.0
+// console.log(logo.dataset.versionNumber); // 3.0
 
-// smooth scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+const h1 = document.querySelector('h1');
+// const alertH1 = () => {
+//   console.log('addEventListener: You are reading the heading!');
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+// h1.addEventListener('mouseenter', alertH1);
 
-btnScrollTo.addEventListener('click', function (e) {
-  // const s1coords = section1.getBoundingClientRect();
-  // console.log(s1coords);
-  // console.log(window.pageYOffset);
-  // console.log(window.scrollY); // viewport Y offset in px
+// h1.onmouseenter = function (e) {
+//   console.log('addEventListener: You are reading the heading!');
+// };
 
-  // window.scrollTo({
-  //   left: s1coords.left,
-  //   top: s1coords.top + window.scrollY,
-  //   behavior: 'smooth',
-  // });
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+// EVENT BUBBLING, PROPAGATION AND DELEGATION
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor());
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // console.log('Event happened on', e.target);
+//   console.log('Event fired on', e.currentTarget.classList);
+//   // e.stopPropagation();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor(); //
+//   // console.log('Event happened on', e.target);
+//   console.log('Event fired on', e.currentTarget.classList);
+// });
+
+// document.querySelector('.nav').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     // console.log('Event happened on', e.target);
+//     console.log('Event fired on', e.currentTarget.classList);
+//   },
+//   true
+// );
