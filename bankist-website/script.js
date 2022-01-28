b99565a4bca8cb3a17cf83db5e75f2292b4f8991
NSokolov114 <sokoloff114@gmail.com>
2022-01-28 23:59:16 +0300

    feat lazy loading images

--- bankist-website/script.js
+++ bankist-website/script.js
@@ -143,7 +143,6 @@ headerObserver.observe(header);
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
@@ -157,6 +156,26 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+///////////////////////////////////////
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ////////////////////////////////////////////
 /////////// EXPERIMENTATION ////////////////
