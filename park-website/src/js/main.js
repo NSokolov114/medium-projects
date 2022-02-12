9b58ba8831fa004bd2401ebcd1da7f7c0c2388ec
NSokolov114 <sokoloff114@gmail.com>
2022-02-12 20:56:58 +0300

    feat map for the park website

--- park-website/src/js/main.js
+++ park-website/src/js/main.js
@@ -1,12 +1,14 @@
 import { picker } from './litepicker.js';
 import { controlGalleryImgs } from './gallery.js';
 import { createNotification } from './helper.js';
+import { initMap } from './map.js';
 
 console.log('hello world');
 
 function init() {
   controlGalleryImgs();
   createNotification('Page is loaded', 'success'); // temp
+  initMap();
 }
 
 init();
---
+++ park-website/src/js/map.js
@@ -0,0 +1,80 @@
+import * as L from 'leaflet';
+import { createNotification } from './helper.js';
+
+const sectionAbout = document.querySelector('.about');
+const overviewLocationBtns = document.querySelectorAll(
+  '.overview__location-btn'
+);
+
+const locations = [
+  { description: 'Ski Park Reception', coords: [49.494, 18.472] },
+  { description: 'Ursa Major Cabins', coords: [49.495, 18.426] },
+  { description: 'Spruce Cabins', coords: [49.5, 18.45] },
+  { description: 'Fire Cabins', coords: [49.481, 18.456] },
+  { description: 'Bear Meadow Campground', coords: [49.498, 18.457] },
+  { description: 'Leadfoot Campground', coords: [49.485, 18.467] },
+  { description: 'Lakeside Campground', coords: [49.509, 18.426] },
+];
+
+// map initiation
+const myMap = L.map('mapid', {
+  center: locations[0].coords,
+  zoom: 13,
+  scrollWheelZoom: false,
+});
+
+function createMarker(coords, text) {
+  const marker = L.marker(coords).addTo(myMap);
+
+  marker.bindPopup(`${text}`);
+  return marker;
+}
+
+function centerMap() {
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(myMap);
+}
+
+// add entrance marker
+function addEntranceMarker() {
+  const markerEntrance = createMarker(
+    locations[0].coords,
+    locations[0].description
+  ).openPopup();
+
+  markerEntrance._icon.classList.add('entranceIcon');
+}
+
+// scroll to map section and show marker when clicking on hotel location
+function addLodgingsMarker() {
+  overviewLocationBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+      sectionAbout.scrollIntoView({ behavior: 'smooth' });
+      createMarker(
+        locations[idx + 1].coords,
+        locations[idx + 1].description
+      ).openPopup();
+      createNotification('Lodging marker is added to the map', 'info');
+    });
+  });
+}
+
+// toggle mouse middle wheel zoom when using the map
+function toggleZoom() {
+  myMap.on('focus', function () {
+    myMap.scrollWheelZoom.enable();
+  });
+  myMap.on('blur', function () {
+    myMap.scrollWheelZoom.disable();
+  });
+}
+
+export function initMap() {
+  centerMap();
+  addEntranceMarker();
+  addLodgingsMarker();
+  toggleZoom();
+}
